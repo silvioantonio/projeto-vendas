6af170f591ed6117913e0d760ba02c47c1c54473
silvioantonio <oliveira_0000@hotmail.com>
2025-05-08 01:10:26 -0300

    Cria arquivo de migracao

--- template/backend/src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
@@ -36,9 +36,10 @@ public class YourDbContextFactory : IDesignTimeDbContextFactory<DefaultContext>
         var connectionString = configuration.GetConnectionString("DefaultConnection");
 
         builder.UseNpgsql(
-               connectionString,
-               b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.WebApi")
+            connectionString,
+            b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM") 
         );
+
 
         return new DefaultContext(builder.Options);
     }
