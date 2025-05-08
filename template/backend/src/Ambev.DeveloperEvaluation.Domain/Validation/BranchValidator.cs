5194345b17cb98688c30bea5f9587c8d7559b933
silvioantonio <oliveira_0000@hotmail.com>
2025-05-07 21:56:23 -0300

    Adiciona entidades e validadores na camada de dominio

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Validation/BranchValidator.cs
@@ -0,0 +1,16 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation
+{
+    public class BranchValidator : AbstractValidator<Branch>
+    {
+        public BranchValidator()
+        {
+            RuleFor(branch => branch.Name)
+                .NotEmpty()
+                .MinimumLength(3).WithMessage("Username must be at least 3 characters long.")
+                .MaximumLength(50).WithMessage("Username cannot be longer than 50 characters.");
+        }
+    }
+}
