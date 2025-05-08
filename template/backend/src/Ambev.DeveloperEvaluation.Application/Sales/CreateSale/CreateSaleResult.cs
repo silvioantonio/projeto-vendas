aae8c9ade8f35b6df9c442c91241ffa668281c83
silvioantonio <oliveira_0000@hotmail.com>
2025-05-08 00:32:07 -0300

    Cria results para sales

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleResult.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleResult.cs
@@ -2,5 +2,11 @@
 {
     public class CreateSaleResult
     {
+        public Guid SaleId { get; set; }
+        public string SaleNumber { get; set; } = string.Empty;
+        public DateTime SaleDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<SaleItemResult> Items { get; set; } = [];
     }
+
 }
