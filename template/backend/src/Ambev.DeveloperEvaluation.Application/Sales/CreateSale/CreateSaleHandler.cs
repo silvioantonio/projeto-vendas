0d005a090c2271fb2180ae3a565cfc0350b869ed
silvioantonio <oliveira_0000@hotmail.com>
2025-05-07 23:16:15 -0300

    Adiciona validações ao handler

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleHandler.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
 using AutoMapper;
 using FluentValidation;
 using MediatR;
@@ -11,7 +12,7 @@ namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale
     /// <param name="saleRepository">The sale repository</param>
     /// <param name="mapper">The AutoMapper instance</param>
     /// <param name="validator">The validator for CreateSaleCommand</param>
-    public class CreateSaleHandler(ISaleRepository saleRepository, IMapper mapper) : IRequestHandler<CreateSaleCommand, CreateSaleResult>
+    public class CreateSaleHandler(ISaleRepository saleRepository, IUserRepository userRepository, IMapper mapper) : IRequestHandler<CreateSaleCommand, CreateSaleResult>
     {
 
         /// <summary>
@@ -27,6 +28,31 @@ namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale
 
             if (!validationResult.IsValid)
                 throw new ValidationException(validationResult.Errors);
+
+            var existingUser = await userRepository.GetByIdAsync(command.CustomerId, cancellationToken) ?? throw new InvalidOperationException($"User with ID {command.CustomerId} does not exist");
+            
+            var existingBranch = await userRepository.GetByIdAsync(command.BranchId, cancellationToken) ?? throw new InvalidOperationException($"Branch with ID {command.BranchId} does not exist");
+            
+            if (command.Items == null || command.Items.Count == 0)
+                throw new InvalidOperationException("Sale must have at least one item.");
+
+            foreach (var item in command.Items)
+            {
+                //TODO: Validar se id do produto existe no banco de dados.
+                if (item.ProductId == Guid.Empty)
+                    throw new InvalidOperationException("Product ID cannot be empty.");
+                if (item.Quantity <= 0 || item.Quantity > 20)
+                    throw new InvalidOperationException("Quantity must be between 1 and 20.");
+                if (item.UnitPrice < 0)
+                    throw new InvalidOperationException("Unit price must be greater than or equal to 0.");
+
+                item.ApplyDiscount();
+                item.Total = item.TotalCalculation();
+            }
+
+            var calculatedTotal = command.Items.Sum(item => item.Total);
+            if (command.TotalAmount != calculatedTotal)
+                throw new InvalidOperationException("Total amount is inconsistent with the sum of item totals.");
 
             var sale = mapper.Map<Sale>(command);
 
