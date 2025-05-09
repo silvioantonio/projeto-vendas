7b419b75759984380d3462fc990eb4a31beff468
silvioantonio <oliveira_0000@hotmail.com>
2025-05-09 00:51:03 -0300

    Cria produto na camada de applicacao e repositorio

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Products/CreateProductCommand.cs
@@ -0,0 +1,46 @@
+﻿using Ambev.DeveloperEvaluation.Common.Validation;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Products
+{
+    /// <summary>
+    /// Command for creating a new product.
+    /// </summary>
+    /// <remarks>
+    /// This command is used to capture the required data for creating a product, 
+    /// including username, password, phone number, email, status, and role. 
+    /// It implements <see cref="IRequest{TResponse}"/> to initiate the request 
+    /// that returns a <see cref="CreateProductResult"/>.
+    /// 
+    /// The data provided in this command is validated using the 
+    /// <see cref="CreateProductCommandValidator"/> which extends 
+    /// <see cref="AbstractValidator{T}"/> to ensure that the fields are correctly 
+    /// populated and follow the required rules.
+    /// </remarks>
+    public class CreateProductCommand : IRequest<CreateProductResult>
+    {
+        /// <summary>
+        /// Gets or sets the name of the product to be created.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+        /// <summary>
+        /// Gets or sets the description of product.
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+        /// <summary>
+        /// Gets or sets the price of product.
+        /// </summary>
+        public decimal Price { get; set; }
+
+        public ValidationResultDetail Validate()
+        {
+            var validator = new CreateProductCommandValidator();
+            var result = validator.Validate(this);
+            return new ValidationResultDetail
+            {
+                IsValid = result.IsValid,
+                Errors = result.Errors.Select(o => (ValidationErrorDetail)o)
+            };
+        }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,42 @@
+﻿
+using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    /// <summary>
+    /// Repository interface for Product entity operations
+    /// </summary>
+    public interface IProductRepository
+    {
+        /// <summary>
+        /// Creates a new product in the repository
+        /// </summary>
+        /// <param name="product">The product to create</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The created product</returns>
+        Task<Product> CreateAsync(Product product, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Retrieves a product by their unique identifier
+        /// </summary>
+        /// <param name="id">The unique identifier of the product</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The product if found, null otherwise</returns>
+        Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Retrieves a product by their name
+        /// </summary>
+        /// <param name="name">The identifier of the product</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The product if found, null otherwise</returns>
+        Task<Product?> GetByNameAsync(string name, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Retrieves a list of products by their unique identifier
+        /// </summary>
+        /// <param name="productIds">The list of unique productId</param>
+        /// <returns>The list of ids if found, empty list otherwise</returns>
+        Task<IEnumerable<Guid>> GetExistingProductIdsAsync(List<Guid> productIds, CancellationToken cancellationToken);
+    }
+}
