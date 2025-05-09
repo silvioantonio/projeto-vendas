352a3aaed22f5d72a7eba0bd7df6438ef0a16861
silvioantonio <oliveira_0000@hotmail.com>
2025-05-09 00:52:02 -0300

    Cria branch na camada de repositorio

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Repositories/IBranchRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    /// <summary>
+    /// Repository interface for Branch entity operations
+    /// </summary>
+    public interface IBranchRepository
+    {
+        /// <summary>
+        /// Retrieves a bramch by their unique identifier
+        /// </summary>
+        /// <param name="branchId">The unique identifier of the branch</param>
+        /// <returns>The branch if found, null otherwise</returns>
+        Task<Branch?> GetByIdAsync(Guid branchId, CancellationToken cancellationToken);
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/BranchRepository.cs
@@ -0,0 +1,28 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    /// <summary>
+    /// Implementation of IBranchRepository using Entity Framework Core
+    /// </summary>
+    public class BranchRepository : IBranchRepository
+    {
+        private readonly DefaultContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of BranchRepository
+        /// </summary>
+        /// <param name="context">The database context</param>
+        public BranchRepository(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Branch?> GetByIdAsync(Guid branchId, CancellationToken cancellationToken)
+        {
+            return await _context.Branchs.FirstOrDefaultAsync(o => o.Id == branchId, cancellationToken);
+        }
+    }
+}
