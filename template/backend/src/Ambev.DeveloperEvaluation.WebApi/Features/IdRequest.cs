6a8b816795dcd99d0f25fa96a07c4cd8efe40067
Silvio Antonio de Oliveira Junior <silvio.antonio@semeartech.com.br>
2025-05-12 16:26:46 -0300

    Altera fluxo para extender classe comum

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/IdRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features
+{
+    public class IdRequest
+    {
+        /// <summary>
+        /// The unique identifier
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
