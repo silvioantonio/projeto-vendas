f566f94b02c532ef84c2a9bc5db403ca7a883fa7
Silvio Antonio de Oliveira Junior <silvio.antonio@semeartech.com.br>
2025-05-12 19:46:02 -0300

    Cria classes para a camada de aplicaçao

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/UpdateSale/UpdateSaleItemValidator.cs
@@ -0,0 +1,16 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.UpdateSale
+{
+    public class UpdateSaleItemValidator : AbstractValidator<SaleItem>
+    {
+        public UpdateSaleItemValidator()
+        {
+            RuleFor(x => x.ProductId).NotEmpty().WithMessage("Product ID is required for each item.");
+            RuleFor(x => x.Quantity).GreaterThan(0).WithMessage("Quantity must be greater than 0 for each item.");
+            RuleFor(x => x.UnitPrice).GreaterThanOrEqualTo(0).WithMessage("Unit Price must be greater than or equal to 0 for each item.");
+            RuleFor(x => x.Discount).GreaterThanOrEqualTo(0).WithMessage("Discount must be greater than or equal to 0 for each item.");
+        }
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/UpdateSale/UpdateSaleProfile.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.UpdateSale;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.UpdateSale
+{
+    public class UpdateSaleProfile : Profile
+    {
+        public UpdateSaleProfile()
+        {
+            CreateMap<UpdateSaleCommand, Sale>()
+            .ForMember(dest => dest.SaleDate, opt => opt.MapFrom(src => DateTime.SpecifyKind(src.SaleDate, DateTimeKind.Utc)));
+            CreateMap<Sale, UpdateSaleResult>();
+            CreateMap<SaleItem, UpdateSaleItemResult>();
+        }
+    }
+}
